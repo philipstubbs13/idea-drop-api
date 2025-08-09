338535e71f39ad830543cb86065bd98155fe52b3
Phil Stubbs <philipstubbs13@gmail.com>
2025-08-08 21:23:58 -0500

    first commit

--- server.js
+++ server.js
@@ -1,13 +1,45 @@
+import cookieParser from "cookie-parser";
 import cors from "cors";
 import dotenv from "dotenv";
 import express from "express";
+import connectDB from "./config/db.js";
+import { errorHandler } from "./middleware/errorHandler.js";
+import authRouter from "./routes/authRoutes.js";
+import ideaRouter from "./routes/ideaRoutes.js";
 
 dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT || 8000;
 
-app.use(cors());
+// Connect to MongoDB
+connectDB();
+
+// CORS Config
+const allowedOrigins = ["http://localhost:3000"];
+
+app.use(
+  cors({
+    origin: allowedOrigins,
+    credentials: true,
+  })
+);
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(cookieParser());
+
+// Routes
+app.use("/api/ideas", ideaRouter);
+app.use("/api/auth", authRouter);
+
+// 404 Fallback
+app.use((req, res, next) => {
+  const error = new Error(`Not Found - ${req.originalUrl}`);
+  res.status(404);
+  next(error);
+});
+
+app.use(errorHandler);
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
