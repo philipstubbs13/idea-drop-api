e5e55fe408e21223d9feb2263eafc669ce065370
Phil Stubbs <philipstubbs13@gmail.com>
2025-08-08 21:36:24 -0500

    Added production domain to cors

--- server.js
+++ server.js
@@ -16,7 +16,10 @@ const PORT = process.env.PORT || 8000;
 connectDB();
 
 // CORS Config
-const allowedOrigins = ["http://localhost:3000"];
+const allowedOrigins = [
+  "http://localhost:3000",
+  "https://idea-drop-ui-flame.vercel.app/",
+];
 
 app.use(
   cors({
